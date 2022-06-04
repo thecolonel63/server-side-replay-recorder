8b909f9e1d183aa2aec0d5882b6690944b39a974
Buck Turk <37983248+thecolonel63@users.noreply.github.com>
2022-06-04 11:50:16 -0500

    Remove fabric api dependency, fix bug with config not being loaded properly, bump mod version to 1.0.3

---
+++ src/main/java/com/thecolonel63/serversidereplayrecorder/mixin/MinecraftServerMixin.java
@@ -0,0 +1,24 @@
+package com.thecolonel63.serversidereplayrecorder.mixin;
+
+import com.thecolonel63.serversidereplayrecorder.server.ServerSideReplayRecorderServer;
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.BooleanSupplier;
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin {
+    @Inject(method = "runServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;setupServer()Z"))
+    private void onInitServer(CallbackInfo ci) {
+        ServerSideReplayRecorderServer.init((MinecraftServer)(Object)this);
+    }
+
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;tickWorlds(Ljava/util/function/BooleanSupplier;)V"))
+    private void onStartTick(BooleanSupplier shouldKeepTicking, CallbackInfo ci) {
+        ServerSideReplayRecorderServer.tick();
+    }
+
+}
