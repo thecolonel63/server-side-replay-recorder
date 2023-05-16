77856a59d3dba79216018ec722a1afb22bc9b6f8
mattymatty <matteo.francesconi@freemails.it>
2023-05-17 00:27:43 +0200

    fixup! cleanup and removal of redundant code

--- src/main/java/com/thecolonel63/serversidereplayrecorder/mixin/region/ServerWorldMixin.java
+++ src/main/java/com/thecolonel63/serversidereplayrecorder/mixin/region/ServerWorldMixin.java
@@ -59,7 +59,7 @@ public class ServerWorldMixin implements RegionRecorderWorld {
         );
     }
     @Inject(method = "createExplosion", at = @At(value = "RETURN"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void handleExplosion(Entity entity, DamageSource damageSource, ExplosionBehavior behavior, double x, double y, double z, float power, boolean createFire, World.ExplosionSourceType explosionSourceType, CallbackInfoReturnable<Explosion> cir, Explosion explosion) {
+    private void handleExplosion(Entity entity, DamageSource damageSource, ExplosionBehavior behavior, double x, double y, double z, float power, boolean createFire, Explosion.DestructionType destructionType, CallbackInfoReturnable<Explosion> cir, Explosion explosion) {
         getRegionRecorders().stream().filter(r -> r.region.isInBox(new Vec3d(x,y,z))).forEach(
                 r -> r.onPacket(new ExplosionS2CPacket(x, y, z, power, explosion.getAffectedBlocks(), Vec3d.ZERO))
         );
