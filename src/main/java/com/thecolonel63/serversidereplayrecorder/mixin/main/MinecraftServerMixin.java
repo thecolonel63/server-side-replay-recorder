ca2ad55f5be1668c9670024b56f0727cbde7f417
Bryan1029384756 <23323626+Bryan1029384756@users.noreply.github.com>
2023-07-02 02:25:59 -0500

    Add Support to save regions to config and auto load regions on startup

--- src/main/java/com/thecolonel63/serversidereplayrecorder/mixin/main/MinecraftServerMixin.java
+++ src/main/java/com/thecolonel63/serversidereplayrecorder/mixin/main/MinecraftServerMixin.java
@@ -10,9 +10,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Iterator;
-import java.util.concurrent.CompletableFuture;
 import java.util.function.BooleanSupplier;
 
 @Mixin(MinecraftServer.class)
